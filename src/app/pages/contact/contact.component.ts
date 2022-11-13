13218f311100fefa9dc7ea21a4d1ca38d4dab2eb
Missael Padilla <lmpadillar@gmail.com>
2022-11-12 20:28:25 -0800

    fix deploy

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  lngLat: [number,number] = [-75.981029433568,45.28719674822362];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, AfterViewInit,ElementRef, ViewChild, Input } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-maps',
@@ -7,22 +6,10 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements AfterViewInit {
-
-  @Input() lngLat: [number,number] = [0,0];
-  @ViewChild('mapa') divMapa!: ElementRef;
   coords = environment.coords;
   constructor() { }
 
   ngAfterViewInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
-    const map = new mapboxgl.Map({
-      container: this.divMapa.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: this.lngLat,
-      zoom: 15,
-      interactive: false
-    })
-    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(map);
   }
 
 }
