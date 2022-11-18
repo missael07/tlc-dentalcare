53944a4e206a0fef0406990cd86a875ea0645464
Missael Padilla <lmpadillar@gmail.com>
2022-11-18 14:10:53 -0800

    change icon and title

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -11,10 +11,10 @@ export class PagesComponent implements OnInit, AfterViewInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigateByUrl('/home/doctors')
+    this.router.navigateByUrl('/home/clinic')
   }
   ngAfterViewInit(): void {
-    this.router.navigateByUrl('/home/doctors')
+    this.router.navigateByUrl('/home/clinic')
     
   }
 
